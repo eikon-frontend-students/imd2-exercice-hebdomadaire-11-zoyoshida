445cdff4cb5504d16005559b8216b9647ba87903
zoyoshida <zophia.yoshida@studentfr.ch>
2026-01-12 13:51:16 +0100

    eeh à changer en vrai

--- src/js/main.js
+++ src/js/main.js
@@ -3,13 +3,17 @@ var stat = document.querySelector("#stat")
 var statCurrent = 147
 
 likeBtn.addEventListener("click", function(){
-    likeBtn.classList.toggle("is-activated");
+
     console.log("bop")
 
     if (likeBtn.classList.contains("is-activated")){
+        setTimeout (() => {likeBtn.classList.toggle("is-activated");}, 4000);
         console.log("bip");
         statCurrent += 1;
         console.log(statCurrent)
         stat.textContent = statCurrent; 
+    }
+    else {
+        likeBtn.classList.toggle("is-activated");
     }
 })
